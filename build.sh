c70e3b8a93715137116055fea52ca5e4a9d21c8f
xiaopinzi <lp900219@163.com>
2022-05-13 16:07:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SCRIPT='./build.cake'
 # Install cake.tool
 dotnet tool install --global cake.tool
 export PATH="$PATH:$HOME/.dotnet/tools"
-
+dotnet tool restore
 echo "dotnet cake $SCRIPT $@"
 
 dotnet cake $SCRIPT "$@"
